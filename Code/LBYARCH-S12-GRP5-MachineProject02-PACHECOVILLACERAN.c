7ec90af7e3ec39de2edb846c007099d097e3ca2c
vmarissaann <marissa_villaceran@dlsu.edu.ph>
2023-11-09 20:53:21 +0800

    initializing matrix

--- Code/LBYARCH-S12-GRP5-MachineProject02-PACHECOVILLACERAN.c
+++ Code/LBYARCH-S12-GRP5-MachineProject02-PACHECOVILLACERAN.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 int main(){
-	printf("Hello World!");
+	// initializing matrix
+	int matrix[6][6] = {
+      {1, 4, 0, 1, 3, 1},
+      {2, 2, 4, 2, 2, 3},
+      {1, 0, 1, 0, 1, 0},
+	  {1, 2, 1, 0, 2, 2},
+	  {2, 5, 3, 1, 2, 5},
+	  {1, 1, 4, 2, 3, 0}
+    };
 	return 0;
 }
