d119c25652d968eb917bae5a1e53d6030785dd1c
vmarissaann <marissa_villaceran@dlsu.edu.ph>
2023-11-10 13:58:54 +0800

    Initialize loops for moving arrays

--- Code/LBYARCH-S12-GRP5-MachineProject02-PachecoVillaceranSASM.asm
+++ Code/LBYARCH-S12-GRP5-MachineProject02-PachecoVillaceranSASM.asm
@@ -1,6 +1,7 @@
 %include "io.inc"
 section .data
 patternCtr db 0
+rowCtr db 0
 matrix_windowsize db 3
 matrix_size db 6
 
@@ -8,10 +9,27 @@ section .text
 global main
 main:
     ; initializes the number of 3x3 window moves
+    ; (6 - window_size +1 )
     MOV AL, [matrix_size]
     SUB AL, [matrix_windowsize]
     ADD EAX, 1
     MOV [patternCtr], EAX
+    
+    ; temporary counter
+    MOV EAX, 0
+    ; initialize loop counter
+    MOV ECX, [patternCtr]
+    row: ADD EAX, 1 ;replace with arrays moving
+        MOV [rowCtr], ECX
+        MOV CL, [patternCtr]
+        SUB CL, 1
+        
+        col: ADD EAX,1 ;replace with arrays moving
+        loop col
+        MOV ECX, [rowCtr]
+    loop row
+    PRINT_DEC 1, EAX
+    
     
     
     
