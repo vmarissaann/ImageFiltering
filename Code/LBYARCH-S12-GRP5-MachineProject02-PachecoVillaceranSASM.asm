20941de3c6f047d9e1570b93a7738c07fe37e70a
vmarissaann <marissa_villaceran@dlsu.edu.ph>
2023-11-10 19:25:14 +0800

    loop for moving through arrays

--- Code/LBYARCH-S12-GRP5-MachineProject02-PachecoVillaceranSASM.asm
+++ Code/LBYARCH-S12-GRP5-MachineProject02-PachecoVillaceranSASM.asm
@@ -1,39 +1,61 @@
 %include "io.inc"
 section .data
+
 patternCtr db 0
-rowCtr db 0
+matrixRowCtr db 0
+matrixColCtr db 0
 matrix_windowsize db 3
 matrix_size db 6
+
+; for window
+windowCtr db 0
+windowRowCtr db 0
+
 ;temporary matrix, but must be called from C
 matrix db 1, 4, 0, 1, 3, 1, 2, 2, 4, 2, 2, 3,1, 0, 1, 0, 1, 0, 1, 2, 1, 0, 2, 2, 2, 5, 3, 1, 2, 5, 1, 1, 4, 2, 3, 0
+matrix_new db 1, 4, 0, 1, 3, 1, 2, 2, 4, 2, 2, 3,1, 0, 1, 0, 1, 0, 1, 2, 1, 0, 2, 2, 2, 5, 3, 1, 2, 5, 1, 1, 4, 2, 3, 0
 
-section .bss
-matrix_new resb 36 ;Reserve 36 bytes for matrix_new
 
 
 section .text
 global main
 main:
+    ;initialize empty counter
+    MOV ECX, 0x0
     ; initializes the number of 3x3 window moves
     ; (6 - window_size +1 )
     MOV AL, [matrix_size]
     SUB AL, [matrix_windowsize]
-    ADD EAX, 1
-    MOV [patternCtr], EAX
+    ADD AL, 1
+    MOV [patternCtr], AL
     
-    ; temporary counter
-    MOV EAX, 0
     ; initialize loop counter
-    MOV ECX, [patternCtr]
-    row: ADD EAX, 1 ;replace with arrays moving
-        MOV [rowCtr], ECX
-        MOV CL, [patternCtr]
-        SUB CL, 1
+    MOV CL, [patternCtr]
+    
+    matrix_row: 
+        ;stores the counter for row of matrix in rowCtr as ECX is initialized for matrix column loop
+        MOV [matrixRowCtr], CL
         
-        col: ADD EAX,1 ;replace with arrays moving
-        loop col
-        MOV ECX, [rowCtr]
-    loop row
+        ;CL initialized for matrix column loop
+        MOV CL, [patternCtr]
+        matrix_col: 
+                   ;stores the counter for column  of matrix in colCtr as ECX as initialized for sample window loop
+                   MOV [matrixColCtr], CL
+                   
+                   ;CL initialized for ROW sample window loop
+                   MOV CL, [matrix_windowsize]
+                   ;temporary counter just to check if it passes through window_row 3 times
+                   MOV EAX, 0
+                   window_row: ADD EAX,1 ; replace with array kineme later
+                              loop window_row
+                   ;put back matrix col counter
+                   MOV CL, [matrixColCtr]   
+                 
+                  
+        loop matrix_col
+        ; put back the counter for matrix row counter
+        MOV CL, [matrixRowCtr]
+    loop matrix_row
     PRINT_DEC 1, EAX
     
     
