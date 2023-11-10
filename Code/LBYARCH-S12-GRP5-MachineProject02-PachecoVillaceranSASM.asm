8e0ddbb92a6e7635585aa73a020e72a4e19d125a
vmarissaann <marissa_villaceran@dlsu.edu.ph>
2023-11-10 14:02:57 +0800

    initialize matrix

--- Code/LBYARCH-S12-GRP5-MachineProject02-PachecoVillaceranSASM.asm
+++ Code/LBYARCH-S12-GRP5-MachineProject02-PachecoVillaceranSASM.asm
@@ -4,6 +4,10 @@ patternCtr db 0
 rowCtr db 0
 matrix_windowsize db 3
 matrix_size db 6
+;temporary matrix, but must be called from C
+matrix db 1, 4, 0, 1, 3, 1, 2, 2, 4, 2, 2, 3,1, 0, 1, 0, 1, 0, 1, 2, 1, 0, 2, 2, 2, 5, 3, 1, 2, 5, 1, 1, 4, 2, 3, 0
+
+
 
 section .text
 global main
